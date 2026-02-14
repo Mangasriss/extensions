c219af0b15298cae6890540db446b442f9ba9911
MangaBot <scan.manga.riss@gmail.com>
2026-02-15 02:26:11 +0300

    Bump Manga Moins versionCode to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "eu.kanade.tachiyomi.extension.fr.mangamoins"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "${libs.versions.extlib.get().substringBeforeLast('.')}.$versionCode"
         base {
             archivesName = "tachiyomi-fr.mangamoins-v$versionName"
